787034c2683124f4f3b1958bdfe4b3d7c8063a44
Igor Villota <167247211+OceanoTch-Innova@users.noreply.github.com>
2026-04-11 15:08:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Esperar a que cargue la página
+document.addEventListener("DOMContentLoaded", function() {
+
+    const boton = document.getElementById("btnContacto");
+
+    boton.addEventListener("click", function() {
+        alert("Gracias por tu interés. Pronto nos pondremos en contacto contigo.");
+    });
+
+});
